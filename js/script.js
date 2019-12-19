514bb2c3677ba9e77c9c31dde45b274deab9802d
Marco <marco.carone@libero.it>
2019-12-19 17:13:10 +0100

    inizio sistemazione dell'interfaccia

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ btnGenera.addEventListener("click",
     var chilometri = inputKm.value;
     var inputEta = document.getElementById("input-eta");
     var eta = inputEta.value;
+    var carrozza = Math.floor(Math.random() * 10);
+    var codiceCp = Math.floor(Math.random() * (100000 - 90000) ) + 90000;
     var tipologiaSconto = "Tarriffa Standard";
     var costoUnChilometro = 0.21;
     // stabilisco il costo del biglietto in riferimento ai km inseriti dall'utente
@@ -17,16 +19,27 @@ btnGenera.addEventListener("click",
     if (eta == "Minorenne") {
       costoBiglietto = costoBiglietto - scontoVenti;
       tipologiaSconto = "Sconto minorenne";
-
     } else if (eta == "Over65") {
       costoBiglietto = costoBiglietto - scontoQuaranta;
       tipologiaSconto = "Sconto Silver";
-
     }
-
     document.getElementById("costo-biglietto").innerHTML = costoBiglietto.toFixed(2);
     document.getElementById("nome-passeggero").innerHTML = nomeCognome;
     document.getElementById("tipologia-sconto").innerHTML = tipologiaSconto;
+    document.getElementById("carrozza").innerHTML = carrozza;
+    document.getElementById("codice-cp").innerHTML = codiceCp;
+    document.getElementById("box-biglietto").classList.remove ("hidden");
+    document.getElementById("box-biglietto").classList.add ("visibility");
+  }
+);
 
+var btnAnnulla = document.getElementById("annulla");
+btnAnnulla.addEventListener("click",
+  function() {
+    document.getElementById("input-nome").value = "";
+    document.getElementById("input-km").value = "";
+    document.getElementById("input-eta").value = "";
+    document.getElementById("box-biglietto").classList.add ("hidden");
+    document.getElementById("box-biglietto").classList.remove ("visibility");
   }
 );
